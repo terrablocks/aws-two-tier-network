677f23e56e6b977d426d419c63174a85e65cf49a
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2020-07-06 13:12:25 +0530

    update output

--- output.tf
+++ output.tf
@@ -23,29 +23,29 @@ output "private_subnet_cidrs" {
 }
 
 output "nat_public_ip" {
-  value = join(", ", aws_nat_gateway.nat_gw.*.public_ip) == "" ? null : join(", ", aws_nat_gateway.nat_gw.*.public_ip)
+  value = var.create_nat ? join(", ", aws_nat_gateway.nat_gw.*.public_ip) : null
 }
 
 output "internal_sg" {
-  value = join(", ", aws_security_group.int_sg.*.id) == "" ? null : join(", ", aws_security_group.int_sg.*.id)
+  value = var.create_sgs ? join(", ", aws_security_group.int_sg.*.id) : null
 }
 
 output "ssh_only_sg" {
-  value = join(", ", aws_security_group.ssh_sg.*.id) == "" ? null : join(", ", aws_security_group.ssh_sg.*.id)
+  value = var.create_sgs ? join(", ", aws_security_group.ssh_sg.*.id) : null
 }
 
 output "public_web_dmz_sg" {
-  value = join(", ", aws_security_group.pub_sg.*.id) == "" ? null : join(", ", aws_security_group.pub_sg.*.id)
+  value = var.create_sgs ? join(", ", aws_security_group.pub_sg.*.id) : null
 }
 
 output "private_web_dmz_sg" {
-  value = join(", ", aws_security_group.pvt_sg.*.id) == "" ? null : join(", ", aws_security_group.pvt_sg.*.id)
+  value = var.create_sgs ? join(", ", aws_security_group.pvt_sg.*.id) : null
 }
 
 output "private_zone_id" {
-  value = join(", ", aws_route53_zone.private.*.zone_id) == "" ? null : join(", ", aws_route53_zone.private.*.zone_id)
+  value = var.create_private_zone ? join(", ", aws_route53_zone.private.*.zone_id) : null
 }
 
 output "private_zone_ns" {
-  value = length(aws_route53_zone.private.*.name_servers) == 0 ? null : aws_route53_zone.private.*.name_servers
+  value = var.create_private_zone ? aws_route53_zone.private.*.name_servers : null
 }
